5faa1fc14e8eba7b8da554636753ffd8cc89698d
Gareth Smyth <gareth-smyth@users.noreply.github.com>
2024-03-12 19:51:11 +0000

    Updated build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'src/main.ts',
@@ -7,5 +9,5 @@ export default {
         format: 'umd',
         name: 'duckDartsGames',
     },
-    plugins: [typescript()],
+    plugins: [nodeResolve(), commonjs(), typescript()],
 };
